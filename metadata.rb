71f75cc7655325f1c40ff195222b21dd86162acb
Adar Porat <aporat@users.noreply.github.com>
2013-10-22 11:09:12 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Adar Porat"
 maintainer_email "adar.porat@gmail.com"
 license           "Apache 2.0"
 description      "Installs and php 5.5"
-version          "0.0.1"
+version          "0.0.2"
 
 depends "build-essential"
 depends "apt"
